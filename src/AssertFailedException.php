bd2b39db5fd988be51181f87198133bd17a42419
Shani Elharrar <atnt123@bezeqint.net>
2011-12-09 12:12:19 +0200

    AssertFailedException __ToString is more likely

--- src/AssertFailedException.php
+++ src/AssertFailedException.php
@@ -10,4 +10,46 @@ class AssertFailedException extends \Exception
         parent::__construct($message);
     }
     
+    private function GetSlicedStack()
+    {
+        $stack = $this->getTrace();
+
+        for ($i = count($stack); $i--; $i >= 0)
+        {
+            $frame = $stack[$i];
+            if (array_key_exists('class', $frame) &&
+                $frame['class'] === 'pUnit\Assert')
+            {
+                // Where to start from is here.
+                $stack = array_slice($stack, 1, $i + 1);
+                break;
+            }    
+        }
+        
+        return $stack;
+    }
+    
+    public function __ToString()
+    {
+        $stack = $this->GetSlicedStack();
+        $retVal = '';
+        
+        foreach ($stack as $key => $frame)
+        {
+            if (array_key_exists('class', $frame))
+            {
+                $class = $frame['class'] . '->';
+            }
+            else
+            {
+                $class = '';
+            }
+            
+            $retVal .= "($key) $class{$frame['function']} \r\n";
+        }
+        
+        
+        return $this->getMessage() . ", Stack Trace : \r\n" . $retVal;
+    }
+    
 }
