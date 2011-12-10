06fe5c6e8968be9cabe6237f5ecb639b6949744b
Shani Elharrar <atnt123@bezeqint.net>
2011-12-10 11:49:55 +0200

    Added unit tests for test runner

--- tests/TestRunnerTests.php
+++ tests/TestRunnerTests.php
@@ -1,41 +1,218 @@
 <?php
 
-use pUnit\Assert as Assert;
+use \pUnit\Assert as Assert;
+use \Mockery as m;
 
 class TestRunnerTests
 {
-    
-    private $mSetUpCalled = 0;
-    
-    private $mTestCalled = 0;
-    
-    private $mTearDownCalled = 0;
-    
-    public function SetUp()
+    private static function GetMock($interface, $methods)
     {
-        Assert::AreEqual(0, $this->mSetUpCalled);
-        Assert::AreEqual(0, $this->mTestCalled);
-        Assert::AreEqual(0, $this->mTearDownCalled);
+        $methodsHash = array();
+        foreach ($methods as $method)
+        {
+            $methodsHash[$method] = null;
+        }
         
-        $this->mSetUpCalled++;
+        $mock = m::mock($interface, $methodsHash);
+        return $mock;
     }
     
-    public function SetUp_Should_Be_Called_Before_This()
+    private static function GetTestMock($methods)
     {
-        Assert::AreEqual(1, $this->mSetUpCalled);
-        Assert::AreEqual(0, $this->mTestCalled);
-        Assert::AreEqual(0, $this->mTearDownCalled);
-        
-        $this->mTestCalled++;
+        return self::GetMock('\pUnit\Interfaces\ITest', func_get_args());
     }
     
+    private static function GetProviderMock($methods)
+    {
+        return self::GetMock('\pUnit\Interfaces\ITestProvider', func_get_args());        
+    }
+    
+    private static function GetFormatterMock($methods)
+    {
+        return self::GetMock('\pUnit\Interfaces\ITestResultFormatter', func_get_args());
+    }
+    
+    private static function RunWithNullFormatter($test)
+    {
+        $formatter = self::GetFormatterMock('StartSuite', 'EndSuite', 'StartTest', 'EndTest', 'Summarize');
+
+        self::RunWithFormatter($test, $formatter);
+    }
+    
+    private static function RunWithFormatter($test, $formatter)
+    {
+        $runner = new \pUnit\TestRunner();
+        
+        $runner->SetOutput($formatter);
+        $runner->SetTest($test);
+        
+        $runner->Run();
+    }
+
     public function TearDown()
     {
-        Assert::AreEqual(1, $this->mSetUpCalled);
-        Assert::AreEqual(1, $this->mTestCalled);
-        Assert::AreEqual(0, $this->mTearDownCalled);
+        m::close();
+    }
+    
+    // Interaction With ITest
+    
+    public function Should_Call_Test_GetName()
+    {
+        $test = self::GetTestMock('SetUp','TearDown','Test');
+        $test->shouldReceive('GetName')->once();
+    
+        self::RunWithNullFormatter($test);
+    }
+    
+    public function Should_Call_Test_Once()
+    {
+        $test = self::GetTestMock('GetName','TearDown','SetUp');
+        $test->shouldReceive('Test')->once();
+
+        self::RunWithNullFormatter($test);
+    }
+    
+    public function Should_Call_Test_Procedure_In_Right_Order()
+    {
+        $test = self::GetTestMock('GetName');
+        $test->shouldReceive('SetUp')->once()->ordered();
+        $test->shouldReceive('Test')->once()->ordered();
+        $test->shouldReceive('TearDown')->once()->ordered();
+         
+        self::RunWithNullFormatter($test);
+    }
+    
+    public function Should_Not_Call_Test_And_TearDown_If_SetUp_Failed()
+    {
+        $test = self::GetTestMock('GetName');
+        $test->shouldReceive('SetUp')->once()->andThrow(new Exception());
+
+        self::RunWithNullFormatter($test);
+    }
+    
+    public function Should_Call_TearDown_Even_If_Test_Failed()
+    {
+        $test = self::GetTestMock('GetName');
+        $test->shouldReceive('SetUp')->once()->ordered();
+        $test->shouldReceive('Test')->once()->andThrow(new Exception())->ordered();
+        $test->shouldReceive('TearDown')->once()->ordered();
+         
+        self::RunWithNullFormatter($test);
+    }
+    
+    // Interaction With ITestProvider Tests
+    
+    public function Should_Call_Get_Tests()
+    {
+        $test = self::GetTestMock('GetName','SetUp','Test','TearDown');
         
-        $this->mTearDownCalled++;
+        $provider = self::GetProviderMock('GetName');
+        $provider->shouldReceive('GetTests')->once()->andReturn(array($test));
+        $provider->shouldReceive('Count')->once()->andReturn(1);        
+        
+        self::RunWithNullFormatter($provider);
+    }
+    
+    public function Should_Test_Inner_Tests()
+    {
+        $test = self::GetTestMock('GetName');
+        $test->shouldReceive('SetUp')->once()->ordered();
+        $test->shouldReceive('Test')->once()->ordered();
+        $test->shouldReceive('TearDown')->once()->ordered();
+        
+        $provider = self::GetProviderMock('GetName', 'SetUp', 'TearDown');
+        $provider->shouldReceive('GetTests')->once()->andReturn(array($test));
+        $provider->shouldReceive('Count')->once()->andReturn(1);        
+        
+        self::RunWithNullFormatter($provider);
+    }
+    
+    public function Should_Test_Inner_Provider()
+    {
+        $innerProvider = self::GetProviderMock('GetName', 'SetUp', 'TearDown');
+        $innerProvider->shouldReceive('GetTests')->once()->andReturn(array());
+        $innerProvider->shouldReceive('Count')->once()->andReturn(0);        
+        
+        $provider = self::GetProviderMock('GetName', 'SetUp', 'TearDown');
+        $provider->shouldReceive('GetTests')->once()->andReturn(array($innerProvider));
+        $provider->shouldReceive('Count')->once()->andReturn(1);        
+        
+        self::RunWithNullFormatter($provider);
+    }
+    
+    // Interaction With Formatter Tests
+    
+    public function Shouuld_Call_Start_Suite_When_Walking_On_Provider()
+    {
+        $formatter = self::GetFormatterMock('EndSuite','StartTest','EndTest','Summarize');
+        $formatter->shouldReceive('StartSuite')->with('Hey', 1)->once();
+        
+        $provider = self::GetProviderMock('SetUp','TearDown');
+        $provider->shouldReceive('GetTests')->andReturn(array());
+        $provider->shouldReceive('GetName')->andReturn('Hey');
+        $provider->shouldReceive('Count')->andReturn(1);
+        
+        self::RunWithFormatter($provider, $formatter);
+    }
+    
+    public function Shouuld_Call_End_Suite_When_Walking_On_Provider()
+    {
+        $formatter = self::GetFormatterMock('StartSuite','StartTest','EndTest','Summarize');
+        $formatter->shouldReceive('EndSuite')->once();
+        
+        $provider = self::GetProviderMock('SetUp','TearDown', 'GetName');
+        $provider->shouldReceive('GetTests')->andReturn(array());
+        $provider->shouldReceive('Count')->andReturn(0);
+        
+        self::RunWithFormatter($provider, $formatter);
+    }
+    
+    public function Should_Call_Sumarize()
+    {
+        $formatter = self::GetFormatterMock('StartSuite', 'EndSuite', 'StartTest', 'EndTest');
+        $formatter->shouldReceive('Summarize')->once();
+        
+        $provider = self::GetProviderMock('SetUp','TearDown', 'GetName');
+        $provider->shouldReceive('GetTests')->andReturn(array());
+        $provider->shouldReceive('Count')->andReturn(0);
+        
+        self::RunWithFormatter($provider, $formatter);
+    }
+    
+    public function Should_Call_StartTest_When_Got_Test()
+    {
+        $formatter = self::GetFormatterMock('StartSuite', 'EndSuite', 'Summarize', 'EndTest');
+        $formatter->shouldReceive('StartTest')->with('Hey');
+        
+        $test = self::GetTestMock('SetUp','Test','TearDown');
+        $test->shouldReceive('GetName')->andReturn('Hey');
+        
+        self::RunWithFormatter($test, $formatter);
+    }
+    
+    public function Should_Call_EndTest_When_Got_Test()
+    {
+        $formatter = self::GetFormatterMock('StartSuite', 'EndSuite', 'Summarize', 'StartTest');
+        $formatter->shouldReceive('EndTest')->with(true);
+        
+        $test = self::GetTestMock('SetUp','Test','TearDown');
+        $test->shouldReceive('GetName')->andReturn('Hey');
+        
+        self::RunWithFormatter($test, $formatter);
+    }
+    
+    public function Should_Call_With_Exception_When_Got_Failed_Test()
+    {
+        $e = new Exception();
+    
+        $formatter = self::GetFormatterMock('StartSuite', 'EndSuite', 'Summarize', 'StartTest');
+        $formatter->shouldReceive('EndTest')->with(false, $e);
+        
+        $test = self::GetTestMock('SetUp','TearDown');
+        $test->shouldReceive('GetName')->andReturn('Hey');
+        $test->shouldReceive('Test')->andThrow($e);
+        
+        self::RunWithFormatter($test, $formatter);
     }
     
 }
